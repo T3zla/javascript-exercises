c8cd44f5d3d44f0fe0c9db858310569438cc96a1
T3zla <alzettes@yahoo.com>
2025-05-29 19:01:26 +0800

    Finish exercise: removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,  ...removeNum) {
+  return array.filter(item => !removeNum.includes(item));
 };
+    
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
