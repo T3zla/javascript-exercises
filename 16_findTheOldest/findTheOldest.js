4ee1de04b65de3d21e0c2515723c95521a1470e3
T3zla <alzettes@yahoo.com>
2025-05-29 19:08:01 +0800

    Finish exercise: findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,4 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldest = people[0];
+    let age = 0;
+
+    people.forEach(function(person) {
+        let currentAge;
+        
+        if ('yearOfDeath' in person) {
+            currentAge = person.yearOfDeath - person.yearOfBirth;
+        } else {
+            currentAge = 2025 - person.yearOfBirth;
+        }
+
+        person.age = currentAge;
+
+        if (person.age > oldest.age) {
+            oldest = person;
+        }
+    })
+
+    return oldest;
+    
+        
 
 };
 
