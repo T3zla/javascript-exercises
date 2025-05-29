7a82eb5f1d1c5db69f7871323a07c43fbb090d26
T3zla <alzettes@yahoo.com>
2025-05-29 19:03:19 +0800

    Finish exercise: calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,24 +1,42 @@
-const add = function() {
-	
+
+const add = function(num1, num2) {
+  let total = num1 + num2;
+  return total;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  let total = num1 - num2
+  return total;	
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let total = 0;
+  for (let i=0; i < array.length; i++){
+    total = total + array[i];
+  }
+  return total;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  let total = 1;
+  for (let i=0; i < array.length; i++){
+    total = total * array[i];
+  }
+  return total;
 
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  let total = Math.pow(num1,num2);
+  return total;
 };
 
-const factorial = function() {
+const factorial = function(num1) {
+  let total = 1;
+  for (let i = 1; i <= num1; i++){
+    total = total * i;
+  }
+  return total;
 	
 };
 
