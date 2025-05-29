6ef1e0e06b87d059d0022093f41e6fd6220a0f88
T3zla <alzettes@yahoo.com>
2025-05-29 19:01:50 +0800

    Finish exercise: sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,22 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+
+    if (!Number.isInteger(num1) || !Number.isInteger(num2) || num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
+
+    let total = 0;
+
+    if (num1 < num2){
+        for (let i = num1; i <= num2; i++){
+            total += i;  
+        }
+        return total;
+    } else {
+        for (let i = num2; i <= num1; i++){
+            total += i;  
+        }
+        return total;
+    }
 
 };
 
