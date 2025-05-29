0acfd2e22470249fa4b8f7fe82ab0bb74d725a01
T3zla <alzettes@yahoo.com>
2025-05-29 19:05:47 +0800

    Finish exercise: fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(digit) {
+    if (digit == 0){
+        return 0;
+    } else if (digit < 0){
+        return "OOPS";
+    } else {
+        let nextNum = 1;
+        let fib = [0];
+        for (let i = 1; i <= digit; i++ ){
+            fib[i] = nextNum;
+            nextNum = fib[i] +fib[i-1];
+        }
+        return fib[digit];
+    }
 
 };
 
