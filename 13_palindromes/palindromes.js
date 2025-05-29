779e5d76a76d29fa48b4f3fe45805c1c075ef0bd
T3zla <alzettes@yahoo.com>
2025-05-29 19:03:57 +0800

    Finish exercise: palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (ogString) {
+    let reversed = "";
+    ogString = ogString.toLowerCase().replace(/[^a-z0-9]/g, "");
+    for (let i = 0; i < ogString.length; i++){
+        reversed += ogString[ogString.length - i - 1];
+    }
 
+    return ogString === reverse;
 };
 
 // Do not edit below this line
