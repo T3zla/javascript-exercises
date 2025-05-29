2c432f40045cc2887c9d11bba2385970f4e8c645
T3zla <alzettes@yahoo.com>
2025-05-29 19:02:50 +0800

    Finish exercise: tempConversions

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(far) {
+  let celsius = (far - 32) * 5/9;
+  return parseFloat(celsius.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  let fahrenheit = (cel * 9/5) + 32;
+  return parseFloat(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
